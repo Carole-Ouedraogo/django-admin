8da968fbbb39a0f2a14a60ec77342d900a2013bf
carole-ouedraogo <ouedca@gmail.com>
2020-06-22 17:04:42 -0400

    completed initial commt

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Carole/CODEPLATOON/Challenges/week_7/django-admin/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
